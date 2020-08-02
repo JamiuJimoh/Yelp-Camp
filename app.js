a2d46213793f3c093a7d6d9d2b5a46f655ea72a5
Janiu <hyperjay55@gmail.com>
2020-08-02 19:17:06 +0100

    fixing the env variable

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ app.use(indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(process.env.PORT, '0.0.0.0', function() {
+app.listen(process.env.PORT, process.env.IP, function() {
 	console.log('The YelpCamp Server Has Started');
 });
